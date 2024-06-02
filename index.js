c08c294185bbfa2f88221f8900baf77beb4ebdbc
tuyenmon2201 <letuyen22012202@gmail.com>
2024-06-02 16:26:03 +0700

    install pug and create folder views

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ const express = require('express') // Nhúng express vào dự án
 const app = express() // Khởi tạo ứng dụng web sử dụng express
 const port = 3000 // Cổng của website
 
+app.set("views", "./views");
+app.set("view engine", "pug");
+
 app.get("/", (req, res) => {
-  res.send("Trang chủ");
+//   res.send("Trang chủ");
+    res.render("client/pages/home/index");
 });
 
 app.get("/products", (req, res) => {
-  res.send("Trang danh sách sản phẩm");
+    res.render("client/pages/products/index");
 });
 
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+    console.log(`App listening on port ${port}`);
 });
