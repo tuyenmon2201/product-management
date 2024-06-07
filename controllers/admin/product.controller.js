bb3e031675459e93d5b879d2bb51db34926b71c3
tuyenmon2201 <letuyen22012202@gmail.com>
2024-06-07 15:55:20 +0700

    Filtering feature by status - backend

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,10 +1,15 @@
 const Product = require("../../models/product.model");
 
 module.exports.index = async (req, res) => {
-
-    const products = await Product.find({
+    const find = {
         deleted: false
-    });
+    }
+
+    if(req.query.status){
+        find.status = req.query.status;
+    }
+
+    const products = await Product.find(find);
 
     console.log(products);
 
