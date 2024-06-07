e9bb8898ceafeb8737a47207d033709107624cf5
tuyenmon2201 <letuyen22012202@gmail.com>
2024-06-07 15:26:17 +0700

    Get product data printed on the interface

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,5 +1,15 @@
-module.exports.index = (req, res) => {
+const Product = require("../../models/product.model");
+
+module.exports.index = async (req, res) => {
+
+    const products = await Product.find({
+        deleted: false
+    });
+
+    console.log(products);
+
     res.render("admin/pages/products/index", {
-        pageTitle: "Trang quản lí sản phẩm"
+        pageTitle: "Trang quản lí sản phẩm",
+        products: products
     });
 }
