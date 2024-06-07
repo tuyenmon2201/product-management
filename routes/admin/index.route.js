c709b00f81ca43f6b3ea981142e43084aa0ea7c6
tuyenmon2201 <letuyen22012202@gmail.com>
2024-06-07 13:53:37 +0700

    Create product interface page

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,6 +1,8 @@
 const dashboardRoute = require("./dashboard.route");
+const productRoute = require("./product.route");
 
 module.exports.index = (app) => {
     app.use("/admin/dashboard", dashboardRoute);
+    app.use("/admin/products", productRoute);
 
 }
