c89304bbb32be6b0e902166622e5eacbf199ca42
tuyenmon2201 <letuyen22012202@gmail.com>
2024-06-12 22:19:42 +0700

    Search feature

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -9,12 +9,22 @@ module.exports.index = async (req, res) => {
         find.status = req.query.status;
     }
 
+    //Tim kiem
+    let keyword = "";
+    if(req.query.keyword){
+        const regex = new RegExp(req.query.keyword, "i");
+        find.title = regex;
+        keyword = req.query.keyword;
+    }
+    //End Tim kiem
+
     const products = await Product.find(find);
 
     console.log(products);
 
     res.render("admin/pages/products/index", {
         pageTitle: "Trang quản lí sản phẩm",
-        products: products
+        products: products,
+        keyword: keyword
     });
 }
