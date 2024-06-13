902624586c4f1733b2089feadf02623086e345c1
tuyenmon2201 <letuyen22012202@gmail.com>
2024-06-13 15:05:09 +0700

    Pagination feature - frontend

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -45,3 +45,18 @@ if(formSearch){
     });
 }
 //End Form Search
+
+//Pagination
+const listButtonPagination = document.querySelectorAll("[button-pagination]");
+if(listButtonPagination.length > 0){
+    let url = new URL(window.location.href);
+    listButtonPagination.forEach(button => {
+        button.addEventListener("click", () => {
+            const page = button.getAttribute("button-pagination");
+            url.searchParams.set("page", page);
+            // console.log(button);
+            window.location.href = url.href;
+        });
+    });
+}
+//End Pagination
