3f793a2db20cce35c0a016ce4838cb908f1ebd92
tuyenmon2201 <letuyen22012202@gmail.com>
2024-07-12 13:40:59 +0700

    Feature create products - Preview image

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -220,3 +220,18 @@ if(showAlert){
     }, 3000);
 }
 // End show alert
+
+// Upload image
+const uploadImage = document.querySelector("[upload-image]");
+if(uploadImage){
+    const uploadImageInput = uploadImage.querySelector("[upload-image-input]");
+    const uploadImagePreview = uploadImage.querySelector("[upload-image-preview]");
+
+    uploadImageInput.addEventListener("change", () => {
+        const file = uploadImageInput.files[0];
+        if(file){
+            uploadImagePreview.src = URL.createObjectURL(file);
+        }
+    });
+}
+// End upload image
