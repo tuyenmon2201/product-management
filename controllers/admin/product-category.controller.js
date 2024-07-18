4cdaeab810ac4c13c0d665f76f142538077dd0c9
tuyenmon2201 <letuyen22012202@gmail.com>
2024-07-18 11:49:35 +0700

    Build product category management (Display category list)

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -1,9 +1,14 @@
 const ProductCategory = require("../../models/product-category.model");
 const systemConfig = require("../../config/system");
 
-module.exports.index = (req, res) => {
+module.exports.index = async (req, res) => {
+    const records = await ProductCategory.find({
+        deleted: false
+    });
+    console.log(records);
     res.render("admin/pages/products-category/index", {
-        pageTitle: "Danh mục sản phẩm"
+        pageTitle: "Danh mục sản phẩm",
+        records: records
     });
 }
 
