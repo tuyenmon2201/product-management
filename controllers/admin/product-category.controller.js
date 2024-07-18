d13eb77f6b6e3b76549d85c1dc9deff9dd20aceb
tuyenmon2201 <letuyen22012202@gmail.com>
2024-07-18 17:06:44 +0700

    Feature edit product category

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -42,3 +42,49 @@ module.exports.createPost = async (req, res) => {
 
     res.redirect(`/${systemConfig.prefixAdmin}/products-category`);
 }
+
+module.exports.edit = async (req, res) => {
+    const id = req.params.id;
+
+    const category = await ProductCategory.findOne({
+        _id: id,
+        deleted: false
+    });
+
+    const categories = await ProductCategory.find({
+        deleted: false
+    });
+
+    const newCategories = createTreeHelper(categories);
+
+    // console.log(newCategories);
+    console.log(category);
+
+    res.render("admin/pages/products-category/edit", {
+        pageTitle: "Chỉnh sửa danh mục sản phẩm",
+        categories: newCategories,
+        category: category
+    });
+}
+
+module.exports.editPatch = async (req, res) => {
+
+    const id = req.params.id;
+    
+    if (req.body.position) {
+        req.body.position = parseInt(req.body.position);
+    }
+    else {
+        const countCategory = await ProductCategory.countDocuments({});
+        req.body.position = countCategory + 1;
+    }
+
+    await ProductCategory.updateOne({
+        _id: id,
+        deleted: false
+    }, req.body);
+
+    req.flash("success", "Cập nhật danh mục thành công");
+
+    res.redirect(`back`);
+}
