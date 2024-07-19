05211790914655d753d1c1c3052df91b951ee763
tuyenmon2201 <letuyen22012202@gmail.com>
2024-07-19 12:30:22 +0700

    Build role group edit

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -26,3 +26,39 @@ module.exports.createPost = async (req, res) => {
 
     res.redirect(`/${systemConfig.prefixAdmin}/roles`);
 }
+
+module.exports.edit = async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        const record = await Role.findOne({
+            _id: id,
+            deleted: false
+        });
+
+        res.render("admin/pages/roles/edit", {
+            pageTitle: "Chỉnh sửa nhóm quyền",
+            record: record
+        });
+    } catch (error) {
+        res.redirect(`/${systemConfig.prefixAdmin}/roles`); 
+    }
+}
+
+module.exports.editPatch = async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        await Role.updateOne({
+            _id: id,
+            deleted: false
+        }, req.body);
+
+        req.flash("success", "Cập nhật thành công!");
+
+        res.redirect("back");
+    } catch (error) {
+        req.flash("success", "Cập nhật thất bại!");
+        res.redirect(`/${systemConfig.prefixAdmin}/roles`); 
+    }
+}
