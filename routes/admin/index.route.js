d23c06e6b85f22bf5330ae9ab848c5c16bafcdff
tuyenmon2201 <letuyen22012202@gmail.com>
2024-07-19 12:02:39 +0700

    Build role group list

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,6 +1,7 @@
 const dashboardRoute = require("./dashboard.route");
 const productRoute = require("./product.route");
 const productCategoryRoute = require("./product-category.route");
+const rolesRoute = require("./role.route");
 const systemConfig = require("../../config/system");
 
 module.exports.index = (app) => {
@@ -9,5 +10,6 @@ module.exports.index = (app) => {
     app.use(`${path}/dashboard`, dashboardRoute);
     app.use(`${path}/products`, productRoute);
     app.use(`${path}/products-category`, productCategoryRoute);
+    app.use(`${path}/roles`, rolesRoute);
 
 }
