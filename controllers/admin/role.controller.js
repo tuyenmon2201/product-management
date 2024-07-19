01f2da2f7382d3b4a72193495627c26156cfacf9
tuyenmon2201 <letuyen22012202@gmail.com>
2024-07-19 12:15:07 +0700

    Build add new role group

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -1,4 +1,5 @@
 const Role = require("../../models/role.model");
+const systemConfig = require("../../config/system");
 
 module.exports.index = async (req, res) => {
     const records = await Role.find({
@@ -9,4 +10,19 @@ module.exports.index = async (req, res) => {
         pageTitle: "Nhóm quyền",
         records: records
     });
+}
+
+module.exports.create = async (req, res) => {
+    res.render("admin/pages/roles/create", {
+        pageTitle: "Tạo mới nhóm quyền"
+    });
+}
+
+module.exports.createPost = async (req, res) => {
+    const record = new Role(req.body);
+    await record.save();
+
+    req.flash("success", "Thêm mới nhóm quyền thành công");
+
+    res.redirect(`/${systemConfig.prefixAdmin}/roles`);
 }
