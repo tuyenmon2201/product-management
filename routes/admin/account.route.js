a7742fe61e1ad3f59f8bc5d41ab3729eeafaa9e7
tuyenmon2201 <letuyen22012202@gmail.com>
2024-07-22 11:36:33 +0700

    Build account create page

--- routes/admin/account.route.js
+++ routes/admin/account.route.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const controller = require("../../controllers/admin/account.controller")
+const controller = require("../../controllers/admin/account.controller");
+const multer = require('multer');
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware");
+
+const upload = multer();
 
 router.get("/", controller.index);
+
+router.get("/create", controller.create);
+
+router.post(
+    "/create",
+    upload.single('avatar'),
+    uploadCloud.uploadSingle,
+    controller.createPost
+);
 
 module.exports = router;
