163cd59021768b2c03ab15512ace32c8ea8dfe73
tuyenmon2201 <letuyen22012202@gmail.com>
2024-07-28 21:07:54 +0700

    Display the general menu on the client side

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,7 +1,10 @@
 const homeRoute = require("./home.route");
-const productRoute = require("./product.route")
+const productRoute = require("./product.route");
+const categoryMiddleware = require("../../middlewares/client/category.middleware");
 
 module.exports.index = (app) => {
+    app.use(categoryMiddleware.category);
+    
     app.use("/", homeRoute);
 
     app.use("/products", productRoute);
