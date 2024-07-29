164b71cb09c1291ebc7aeeca69e2c5b7fc2fbafd
tuyenmon2201 <letuyen22012202@gmail.com>
2024-07-29 13:05:47 +0700

    Display list outstanding product and new product

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,5 +1,34 @@
-module.exports.index = (req, res) => {
+const Product = require("../../models/product.model");
+
+module.exports.index = async (req, res) => {
+    const productsFeatured = await Product.find({
+        featured: "1",
+        deleted: false,
+        status: "active"
+    })
+    .sort({position: "desc"})
+    .limit(6)
+    .select("-description");
+
+    for(const item of productsFeatured){
+        item.priceNew = ((1 - item.discountPercentage/100) * item.price).toFixed(0);
+    }
+
+    const productsNew = await Product.find({
+        deleted: false,
+        status: "active"
+    })
+    .sort({position: "desc"})
+    .limit(6)
+    .select("-description");
+
+    for(const item of productsNew){
+        item.priceNew = ((1 - item.discountPercentage/100) * item.price).toFixed(0);
+    }
+
     res.render("client/pages/home/index", {
-        pageTitle: "Trang chủ"
+        pageTitle: "Trang chủ",
+        productsFeatured: productsFeatured,
+        productsNew: productsNew
     });
 }
