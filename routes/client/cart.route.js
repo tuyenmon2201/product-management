0651244582a1629f99662bc5b7f2dcd964073a3e
tuyenmon2201 <letuyen22012202@gmail.com>
2024-08-01 12:47:30 +0700

    Create cart page

--- routes/client/cart.route.js
+++ routes/client/cart.route.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../../controllers/client/cart.controller");
 
+router.get("/", controller.index);
+
 router.post("/add/:productId", controller.addPost);
 
 module.exports = router;
