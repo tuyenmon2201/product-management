c801a5155e123c52d359abcd6e321d813bec0e1e
tuyenmon2201 <letuyen22012202@gmail.com>
2024-08-03 13:10:52 +0700

    Create collection user

---
+++ models/user.model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+// const { Schema } = mongoose;
+
+const userSchema = new mongoose.Schema ({
+    fullName: String,
+    email: String,
+    phone: String,
+    password: String,
+    tokenUser: String,
+    avatar: String,
+    status: {
+        type: String,
+        default: "active"
+    },
+    deleted: {
+        type: Boolean,
+        default: false
+    }
+},  {
+    timestamps: true
+});
+
+const User = mongoose.model("User", userSchema, "users");
+
+module.exports = User;
