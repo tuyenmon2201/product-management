b8bf6f7131a101d2ade59fe9fc12d7a62aa51697
tuyenmon2201 <letuyen22012202@gmail.com>
2024-08-05 14:23:13 +0700

    Forgot password - Guide send OTP to automatic email

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -2,6 +2,7 @@ const md5 = require("md5");
 const generateHelper = require("../../helpers/generate.helper");
 const User = require("../../models/user.model");
 const ForgotPassword = require("../../models/forgot-password.model");
+const sendMailHelper = require("../../helpers/sendEmail.helper");
 
 module.exports.register = async (req, res) => {
     res.render("client/pages/user/register", {
@@ -112,6 +113,9 @@ module.exports.forgotPasswordPost = async (req, res) => {
     await forgotPassword.save();
 
     // Send OTP through email to user
+    const subject = "Mã OTP lấy lại mật khẩu";
+    const htmlSendMail = `Mã OTP xác thực của bạn là <b style="color: green;">${otp}</b>. Mã OTP có hiệu lực trong 3 phút. Vui lòng không cung cấp mã OTP cho người khác.`;
+    sendMailHelper.sendMail(email, subject, htmlSendMail);
 
     res.redirect(`/user/password/otp?email=${email}`);
 }
