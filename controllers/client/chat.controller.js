c06fd2cf11e1bc66105ac365839020510a578988
tuyenmon2201 <letuyen22012202@gmail.com>
2024-08-10 13:23:31 +0700

    Optimized file socket

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,50 +1,11 @@
-const Chat = require("../../models/chat.model");
 const User = require("../../models/user.model");
-const streamUpload = require("../../helpers/streamUpload.helper");
+const Chat = require("../../models/chat.model");
+const chatSocket = require("../../sockets/client/chat.socket");
 
 module.exports.index = async (req, res) => {
-    const userId = res.locals.user.id;
-    const fullName = res.locals.user.fullName;
-    _io.once('connection', (socket) => {
-        // console.log('a user connected', socket.id);
-        // CLIENT_SEND_MESSAGE
-        socket.on("CLIENT_SEND_MESSAGE", async (data) => {
-            const chatData = {
-                userId: userId,
-                content: data.content
-            }
-
-            const linkImages = [];
-
-            for (const image of data.images) {
-                const result = await streamUpload(image);
-                linkImages.push(result.url);
-            }
-
-            chatData.images = linkImages;
-
-            const chat = new Chat(chatData);
-            await chat.save();
-
-            // Return message realtime for people
-            _io.emit("SERVER_RETURN_MESSAGE", {
-                userId: userId,
-                content: data.content,
-                fullName: fullName,
-                images: linkImages
-            });
-        });
-
-        // CLIENT_SEND_TYPING
-        socket.on("CLIENT_SEND_TYPING", (type) => {
-            socket.broadcast.emit("SERVER_RETURN_TYPING", {
-                userId: userId,
-                fullName: fullName,
-                type: type
-            });
-        });
-    });
-
+    // SocketIO
+    chatSocket(req, res);
+    // End SocketIO
     const chats = await Chat.find({});
 
     for (const chat of chats) {
