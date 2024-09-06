800daddab52b0777454140d5dfce30ea8c4c953f
tuyenmon2201 <letuyen22012202@gmail.com>
2024-09-06 22:09:32 +0700

    Create collection rooms-chat

---
+++ models/rooms-chat.model.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+// const { Schema } = mongoose;
+
+const roomChatSchema = new mongoose.Schema ({
+    title: String,
+    // avatar: String,
+    tyleRoom: String,
+    users: [
+        {
+            userId: String,
+            role: String,
+        }
+    ],
+    deleted: {
+        type: Boolean,
+        default: false
+    }
+},  {
+    timestamps: true
+});
+
+const RoomChat = mongoose.model("RoomChat", roomChatSchema, "rooms-chat");
+
+module.exports = RoomChat;
